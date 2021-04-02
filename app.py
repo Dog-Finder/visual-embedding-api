c6db85f389c36f8fc3ca8742e7ae9281905e27e6
Yoav Navon <yanavon@uc.cl>
2021-04-01 22:50:37 -0300

    Add poetry

--- app.py
+++ app.py
@@ -10,21 +10,25 @@ from io import BytesIO
 
 model = InceptionResNetV2(weights='imagenet', include_top=False, pooling='avg')
 
+
 def make_connect():
-    host = 'search-dog-finder-v2-7wjebliu7bqocjgom3d6csaxde.us-east-1.es.amazonaws.com'
+    host = 'search-dog-finder-search-fcdwwfogeqqcr2dyrhjonwqmem.us-east-1.es.amazonaws.com'
+    region = 'us-east-1'  # e.g. us-west-1
+
     service = 'es'
-    region = 'us-east-1'
-    credentials = boto3.Session().get_credentials()
-    awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
+    credentials = boto3.Session(profile_name='elasticsearch').get_credentials()
+    awsauth = AWS4Auth(credentials.access_key, credentials.secret_key,
+                       region, service, session_token=credentials.token)
 
     es = Elasticsearch(
-        hosts = [{'host': host, 'port': 443}],
-        http_auth = awsauth,
-        use_ssl = True,
-        verify_certs = True,
-        connection_class = RequestsHttpConnection
+        hosts=[{'host': host, 'port': 443}],
+        http_auth=awsauth,
+        use_ssl=True,
+        verify_certs=True,
+        connection_class=RequestsHttpConnection
     )
     return es
+
 
 def hello(event, context):
     body = {'message': 'Hello World'}
@@ -34,16 +38,17 @@ def hello(event, context):
     }
     return response
 
+
 def predict(event, context):
     body = json.loads(event['body'])
     url = body['url']
     response = requests.get(url)
     img = Image.open(BytesIO(response.content))
-    resized = img.resize((299,299))
+    resized = img.resize((299, 299))
     array = np.array(resized)[None]
     array = preprocess_input(array)
     prediction = model(array)
-    body =  {'prediction': prediction.numpy().tolist()}
+    body = {'prediction': prediction.numpy().tolist()}
     response = {
         "statusCode": 200,
         "body": json.dumps(body)
@@ -59,7 +64,7 @@ def predict_save(event, context):
     entryId = body['entryId']
     response = requests.get(imageLink)
     img = Image.open(BytesIO(response.content))
-    resized = img.resize((299,299))
+    resized = img.resize((299, 299))
     array = np.array(resized)[None]
     array = preprocess_input(array)
     prediction = model(array).numpy().tolist()[0]
@@ -70,12 +75,13 @@ def predict_save(event, context):
         "entry-id": entryId,
     }
     result = es.index(index='vectors', body=document)
-    body =  {'result': result}
+    body = {'result': result}
     response = {
         "statusCode": 200,
         "body": json.dumps(body)
     }
     return response
+
 
 def search(event, context):
     es = make_connect()
@@ -83,42 +89,44 @@ def search(event, context):
     url = body['url']
     response = requests.get(url)
     img = Image.open(BytesIO(response.content))
-    resized = img.resize((299,299))
+    resized = img.resize((299, 299))
     array = np.array(resized)[None]
     array = preprocess_input(array)
     prediction = model(array).numpy().tolist()[0]
     results = es.search(index='vectors', body={
-    "size": 5,
-    "query": {
-        "knn": {
-            "image-vector": {
-                "vector": prediction,
-                "k": 5
+        "size": 5,
+        "query": {
+            "knn": {
+                "image-vector": {
+                    "vector": prediction,
+                    "k": 5
+                }
             }
-        }
-    },
-    "_source": ["image-url","user-id","entry-id"],
+        },
+        "_source": ["image-url", "user-id", "entry-id"],
     })
-    body =  {'result': results}
+    body = {'result': results}
     response = {
         "statusCode": 200,
         "body": json.dumps(body)
     }
     return response
 
+
 def random(event, context):
-    array = np.random.random((1,299,299,3))
+    array = np.random.random((1, 299, 299, 3))
     prediction = model(array)
-    body =  {'prediction': prediction.numpy().tolist()}
+    body = {'prediction': prediction.numpy().tolist()}
     response = {
         "statusCode": 200,
         "body": json.dumps(body)
     }
     return response
+
 
 def es_route(event, context):
     es = make_connect()
-    index ='vectors'
+    index = 'vectors'
     body = {'index': es.indices.exists(index)}
     response = {
         "statusCode": 200,
