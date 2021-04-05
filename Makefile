33c3c360fd6188f9f564f5b1963ebcb86636293b
yoavnavon <yanavon@uc.cl>
2021-04-04 21:17:25 -0400

    Dev (#10)
    * Feat/elasticsearch (#8)
    
    * Add poetry
    
    * Build container image manually
    
    * Download model in image and warm lambda
    
    * Use image tag and auto deploy
    
    * Fix enable warmup
    
    * Connect to elasticsearch from lambda and local
    
    * Add lock
    
    * Separate lost and found dogs (#9)
    
    * Add CI/CD
    
    * Fix typo
    
    * Add stage to ecr repo
    
    * Use correct uri
    
    * Cleaner uri definition

--- Makefile
+++ Makefile
@@ -1,8 +1,24 @@
 build:
-	docker build -t visual-embedding-api .
+	poetry export -f requirements.txt --output requirements.txt --without-hashes
+	docker build -t visual-embedding-api-${stage} .
+	docker tag visual-embedding-api-${stage}:latest 983570756921.dkr.ecr.us-east-1.amazonaws.com/dog-finder-search-${stage}
+
+deploy-build:
+	docker build -t visual-embedding-api-${stage} .
+	docker tag visual-embedding-api-${stage}:latest 983570756921.dkr.ecr.us-east-1.amazonaws.com/dog-finder-search-${stage}
+
+deploy:
+	aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 983570756921.dkr.ecr.us-east-1.amazonaws.com/dog-finder-search-$(stage)
+	docker push 983570756921.dkr.ecr.us-east-1.amazonaws.com/dog-finder-search-$(stage)
+	sls deploy --stage $(stage)
 
 local:
-	docker run -p 9000:8080 visual-embedding-api:latest $(handler)
+	docker run \
+	-p 9000:8080 \
+	-v "$$HOME/.aws":/root/.aws \
+  -e AWS_REGION=us-east-1 \
+	-e AWS_PROFILE=yoavnavon \
+	visual-embedding-api-dev:latest $(handler)
 
 ping:
 	curl -XPOST "http://localhost:9000/2015-03-31/functions/function/invocations" -d '{}'
--- app.py
+++ app.py
@@ -2,48 +2,72 @@ import json
 import boto3
 from elasticsearch import Elasticsearch, RequestsHttpConnection
 from requests_aws4auth import AWS4Auth
-from tensorflow.keras.applications.inception_resnet_v2 import InceptionResNetV2, preprocess_input
+from tensorflow.keras.applications.inception_resnet_v2 import preprocess_input
 import numpy as np
 import requests
 from PIL import Image
 from io import BytesIO
+import os
 
-model = InceptionResNetV2(weights='imagenet', include_top=False, pooling='avg')
+import tensorflow as tf
+import tensorflow_hub as hub
+
+IMAGE_WIDTH = 299
+IMAGE_HEIGHT = 299
+
+IMAGE_SHAPE = (IMAGE_WIDTH, IMAGE_HEIGHT)
+model = tf.keras.Sequential([hub.KerasLayer("model/", trainable=False)])
+model.build([None, IMAGE_WIDTH, IMAGE_HEIGHT, 3])
+
+# model = InceptionResNetV2(weights='imagenet', include_top=False, pooling='avg')
+
 
 def make_connect():
-    host = 'search-dog-finder-v2-7wjebliu7bqocjgom3d6csaxde.us-east-1.es.amazonaws.com'
+    host = 'search-dog-finder-search-fcdwwfogeqqcr2dyrhjonwqmem.us-east-1.es.amazonaws.com'
+    region = 'us-east-1'  # e.g. us-west-1
     service = 'es'
-    region = 'us-east-1'
-    credentials = boto3.Session().get_credentials()
-    awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
+
+    sts_connection = boto3.client('sts')
+    acct_b = sts_connection.assume_role(
+        RoleArn="arn:aws:iam::111465656160:role/query-elasticsearch",
+        RoleSessionName="cross_acct_lambda"
+    )
+
+    ACCESS_KEY = acct_b['Credentials']['AccessKeyId']
+    SECRET_KEY = acct_b['Credentials']['SecretAccessKey']
+    SESSION_TOKEN = acct_b['Credentials']['SessionToken']
+
+    awsauth = AWS4Auth(ACCESS_KEY, SECRET_KEY,
+                       region, service, session_token=SESSION_TOKEN)
 
     es = Elasticsearch(
-        hosts = [{'host': host, 'port': 443}],
-        http_auth = awsauth,
-        use_ssl = True,
-        verify_certs = True,
-        connection_class = RequestsHttpConnection
+        hosts=[{'host': host, 'port': 443}],
+        http_auth=awsauth,
+        use_ssl=True,
+        verify_certs=True,
+        connection_class=RequestsHttpConnection
     )
     return es
 
+
 def hello(event, context):
-    body = {'message': 'Hello World'}
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
-    return response
+    print(os.environ)
+    return {'message': 'Hello World'}
+
 
 def predict(event, context):
+    if event.get("source") == "serverless-plugin-warmup":
+        print("WarmUp - Lambda is warm!")
+        return {}
     body = json.loads(event['body'])
     url = body['url']
     response = requests.get(url)
     img = Image.open(BytesIO(response.content))
-    resized = img.resize((299,299))
+    resized = img.resize((299, 299))
     array = np.array(resized)[None]
     array = preprocess_input(array)
     prediction = model(array)
-    body =  {'prediction': prediction.numpy().tolist()}
+    body = {'prediction': prediction.numpy().tolist()}
     response = {
         "statusCode": 200,
         "body": json.dumps(body)
@@ -52,14 +76,20 @@ def predict(event, context):
 
 
 def predict_save(event, context):
+    if event.get("source") == "serverless-plugin-warmup":
+        print("WarmUp - Lambda is warm!")
+        return {}
     es = make_connect()
     body = json.loads(event['body'])
     imageLink = body['imageLink']
     userId = body['userId']
     entryId = body['entryId']
+    index_type = 'found' if entryId.startswith('found') else 'lost'
+    index_env = os.getenv('LAMBDA_ENV', 'local')
+    index = f'{index_env}-{index_type}'
     response = requests.get(imageLink)
     img = Image.open(BytesIO(response.content))
-    resized = img.resize((299,299))
+    resized = img.resize((299, 299))
     array = np.array(resized)[None]
     array = preprocess_input(array)
     prediction = model(array).numpy().tolist()[0]
@@ -69,56 +99,72 @@ def predict_save(event, context):
         "user-id": userId,
         "entry-id": entryId,
     }
-    result = es.index(index='vectors', body=document)
-    body =  {'result': result}
+    result = es.index(index=index, body=document)
+    body = {'result': result}
     response = {
         "statusCode": 200,
         "body": json.dumps(body)
     }
     return response
 
+
 def search(event, context):
+    if event.get("source") == "serverless-plugin-warmup":
+        print("WarmUp - Lambda is warm!")
+        return {}
     es = make_connect()
     body = json.loads(event['body'])
     url = body['url']
+    index_type = body['index']
+    index_env = os.getenv('LAMBDA_ENV', 'local')
+    index = f'{index_env}-{index_type}'
+    size = body['size']
     response = requests.get(url)
     img = Image.open(BytesIO(response.content))
-    resized = img.resize((299,299))
+    resized = img.resize((299, 299))
     array = np.array(resized)[None]
     array = preprocess_input(array)
     prediction = model(array).numpy().tolist()[0]
-    results = es.search(index='vectors', body={
-    "size": 5,
-    "query": {
-        "knn": {
-            "image-vector": {
-                "vector": prediction,
-                "k": 5
+    results = es.search(index=index, body={
+        "size": size,
+        "query": {
+            "knn": {
+                "image-vector": {
+                    "vector": prediction,
+                    "k": size
+                }
             }
-        }
-    },
-    "_source": ["image-url","user-id","entry-id"],
+        },
+        "_source": ["image-url", "user-id", "entry-id"],
     })
-    body =  {'result': results}
+    body = {'result': results}
     response = {
         "statusCode": 200,
         "body": json.dumps(body)
     }
     return response
+
 
 def random(event, context):
-    array = np.random.random((1,299,299,3))
+    if event.get("source") == "serverless-plugin-warmup":
+        print("WarmUp - Lambda is warm!")
+        return {}
+    array = np.random.random((1, 299, 299, 3))
     prediction = model(array)
-    body =  {'prediction': prediction.numpy().tolist()}
+    body = {'prediction': prediction.numpy().tolist()}
     response = {
         "statusCode": 200,
         "body": json.dumps(body)
     }
     return response
 
+
 def es_route(event, context):
+    if event.get("source") == "serverless-plugin-warmup":
+        print("WarmUp - Lambda is warm!")
+        return {}
     es = make_connect()
-    index ='vectors'
+    index = 'vectors'
     body = {'index': es.indices.exists(index)}
     response = {
         "statusCode": 200,
